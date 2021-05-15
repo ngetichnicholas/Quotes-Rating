b8800f4853a66f491c2ec8867d6e159006762c90
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-15 10:05:47 +0300

    Add class Quote

---
+++ src/app/quote.ts
@@ -0,0 +1,5 @@
+export class Quote {
+    showInfo: boolean;
+    constructor(public id:number,public name:string,public quote-title:string,public quote-info:string, public datePosted:Date,public upvote:number,public downvote:number)
+    this.showInfo=false;
+}

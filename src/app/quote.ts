bff946b3e3da4aca71fb4fa8309a47769ff7a7f3
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-15 10:32:59 +0300

    Add quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
-    showInfo: boolean;
-    constructor(public id:number,public name:string,public quote-title:string,public quote-info:string, public datePosted:Date,public upvote:number,public downvote:number)
-    this.showInfo=false;
+  showInfo: boolean;
+  constructor(public id:number,public name:string,public quoteTitle:string,public quoteInfo:string,public author:string, public datePosted: Date,public upvote:number, public downvote:number){
+      this.showInfo=false;
+    }
 }

8f21c40ae20feb23115ec2c6867fa994e2617456
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-15 17:45:21 +0300

    Add font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
@@ -18,11 +20,12 @@ import { HighlightDirective } from './highlight.directive';
     QuoteFormComponent,
     DateCountPipe,
     HighlightDirective
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
 
   ],
   providers: [],

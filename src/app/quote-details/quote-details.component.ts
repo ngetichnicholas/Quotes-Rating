932c6addf20f2571044e02f83f75a1df9a83df1e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-15 10:43:34 +0300

    Add quote details component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,27 @@
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'; 
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quoty: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+  deleteQuote(read:boolean){
+    this.isRead.emit(read);
+  }
+  upvote(){
+    this.quoty.upvote+=1;
+  }
+  downvote(){
+    this.quoty.downvote+=1;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

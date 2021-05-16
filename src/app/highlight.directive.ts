c6d4d7a96806014b118934a7fb0e20d3afbd21f0
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-16 20:48:11 +0300

    Add changes to quotes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -7,5 +7,6 @@ export class HighlightDirective {
 
   constructor(elem:ElementRef) {
     elem.nativeElement.style.backgroundColor="lightgreen"
+    elem.nativeElement.style.border="0.5rem solid #198754"
    }
 }

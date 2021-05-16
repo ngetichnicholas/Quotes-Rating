9e828d3f766ea1de968d01436e4ce0f8fe812958
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-16 10:05:58 +0300

    Add changes to quote details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   //showInfo: boolean;
   constructor(public id:number,public name:string,public quoteTitle:string,public quoteInfo:string,public author:string, public datePosted: Date,public upvote:number, public downvote:number){
-     // this.showInfo=true;
-    }
+    // this.showInfo=true;
+   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote (1,'Nicholas','Networking','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2019,7,12),0,0),
-    new Quote (2,'Mutie','Technology','The great myth of our times is that technology is communication.','James Cameron',new Date(2019,7,14),0,0),
-    new Quote (3,'Johnstone','Religion','If you set your goals ridiculously high and it is a failure, you will fail above everyone else success.','Libby Larsen',new Date(2019,6,3),0,0),
-    new Quote (4,'Odero','Literature','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2019,7,7),0,0),
-    new Quote (5,'Kenedy','Computer Programming','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2019,7,17),0,0),
-    new Quote (6,'Aspin','Social Life','The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.','Helen Keller',new Date(2021,4,15),0,0)
+    new Quote (1,'Nicholas','Networking','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2021,4,15),65,5),
+    new Quote (2,'Mutie','Technology','The great myth of our times is that technology is communication.','James Cameron',new Date(2021,4,13),12,3),
+    new Quote (3,'Johnstone','Religion','If you set your goals ridiculously high and it is a failure, you will fail above everyone else success.','Libby Larsen',new Date(2021,4,8),17,5),
+    new Quote (4,'Odero','Literature','Communications tools don’t get socially interesting until they get technologically boring.','Clay Shirky',new Date(2021,4,11),42,6),
+    new Quote (5,'Kenedy','Computer Programming','Programs must be written for people to read, and only incidentally for machines to execute.','Harold Abelson',new Date(2021,4,12),5,12),
+    new Quote (6,'Aspin','Social Life','The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.','Helen Keller',new Date(2021,4,12),40,16)
   ];
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
